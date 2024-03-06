030d4654f01a8490b6b9287f5c12120c8a1540cc
sonachandran <sonachandran2002@gmail.com>
2024-03-06 12:10:34 +0530

    complete

--- app.js
+++ app.js
@@ -8,9 +8,26 @@ const cors = require('cors');
 app.use(express.json())
 app.use(cors())
 const User = require('./models/User')
+const Userblog = require('./models/Userblog');
 const bcrypt = require('bcrypt')
 const saltrounds = 10;
-const jwt=require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const multer=require('multer')
+const path=require('path')
+app.use('/uploads',express.static('uploads'));
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/') 
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname)
+    }
+});
+const upload = multer({ storage: storage });
+
+
+
 app.post('/insert', async (req, res) => {
 
     try {
@@ -18,7 +35,6 @@ app.post('/insert', async (req, res) => {
         // if (existingUser) {
         //     return res.status(400).json({ message: 'User already exists' });
         // }
-
         let hashpassword = await bcrypt.hash(req.body.password, saltrounds)
         console.log(hashpassword);
         let newuserr = new User({ ...req.body, password: hashpassword })
@@ -33,14 +49,32 @@ app.post('/insert', async (req, res) => {
     }
 
 })
+app.post('/insertblog',upload.single('image'), async (req, res) => {
+    try {
+        const imagepath=req.file ? req.file.filename :''
+        const newblog = new Userblog({...req.body,image:imagepath})
+        console.log('newuser',newblog);
+        const response = await newblog.save()
+        res.json(response)
+    }
+    catch (error) {
+        console.error("Error inserting blog:", error);
+        res.status(500).json({ error: 'Server error' });
+    }
+});
 
+app.get('/find',async(req,res)=>{ 
+    let response=await Userblog.find()
+    res.json(response)
+
+})
 
 
 app.post('/login', async (req, res) => {
     const { email, password } = req.body;
     try {
         const user = await User.findOne({ email });//it takes all the data from database
-        console.log("dfgsfr",user);
+        console.log("dfgsfr", user);
 
         if (!user) {
             return res.status(401).json({ error: 'Invalid email or password' });
@@ -51,11 +85,11 @@ app.post('/login', async (req, res) => {
         if (!isMatch) {
             return res.status(401).json({ error: 'Invalid email or password' });
         }
-        
 
-        const token=jwt.sign({id:user.id,username:user.username},'abc');
+
+        const token = jwt.sign({ id: user.id, username: user.username }, 'abc');
         console.log(token);
-        res.json({user,token});
+        res.json({ user, token });
 
     } catch (error) {
         console.error('Login error:', error);
@@ -67,29 +101,27 @@ app.post('/login', async (req, res) => {
 const verifyToken = (req, res, next) => {
     let token = req.headers['authorization'];
     console.log(token);
-    token=token.split(' ')
+    token = token.split(' ')
     console.log(token[1]);
-  
+
     if (!token[1]) {
-      return res.status(403).json({ message: 'Token is not provided' });
+        return res.status(403).json({ message: 'Token is not provided' });
     }
-  
+
     jwt.verify(token[1], 'abc', (err, decoded) => {
-      if (err) {
-        return res.status(401).json({ message: 'Unauthorized: Invalid token' });
-      }
-      req.decoded = decoded;
-      console.log(req.decoded,'asd');
-      next();
+        if (err) {
+            return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+        }
+        req.decoded = decoded;
+        console.log(req.decoded, 'asd');
+        next();
     });
-  };
+};
 
 
+app.get('/find', verifyToken, async (req, res) => {
 
-
-app.get('/find',verifyToken,async(req,res)=>{
-    
-    let response=await User.find()
+    let response = await User.find()
     res.json(response)
 })
 
---
+++ models/Userblog.js
@@ -0,0 +1,28 @@
+const mongoose=require('mongoose')
+const User = require('./User')
+const blogscheme=new mongoose.Schema({
+
+     title:{
+        type:String,
+        required:true
+
+    },
+     description:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String
+    },
+    author:{
+        type:mongoose.Types.ObjectId,
+        ref:User
+    }
+
+    
+    
+})
+
+let Userblog=mongoose.model('blog',blogscheme)
+
+module.exports=Userblog
